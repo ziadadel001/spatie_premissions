3f90ac1b87861f4ee6936395c4e0471bc6577721
ziadadel001 <ziadadel00120@gmail.com>
2025-03-23 23:33:03 +0200

    create a controller for permissions and create methods for create, update and delete

---
+++ app/Http/Controllers/permissionController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class permissionController extends Controller
+{
+    // this method will show permissions page
+    public function index() {}
+
+    // this method will show create permission page
+    public function create() {}
+
+    // this method will insert permission in DB
+    public function store() {}
+    // this method will show edit permission page
+    public function edit() {}
+    // this method will update permission in DB
+    public function update() {}
+
+    // this method will delete permission in DB
+    public function destroy() {}
+}
